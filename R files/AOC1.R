47f7aab60aa29863716ba84fde0ae2e789028beb
Cindy Nicklin <22964603+cnicklin@users.noreply.github.com>
2021-12-03 09:16:18 -0500

    getting closer. for loop is working

--- R files/AOC1.R
+++ R files/AOC1.R
@@ -6,33 +6,13 @@ setwd("C:/Users/cnicklin/Documents/AdventOfCode")
 # for practice we'll use a smaller array
 readings <- data.frame(value=c(176,179,180,195,197,203,211,212,200,225))
 mydata <- data.frame(value=readings[1,1],result=NA)
+numb <- nrow(readings)
 
-vari <- 1
-vari2 <- 2
-
-loopy <- function(vari,
-                  vari2) {
-  num <- readings[vari,1]
-  num2 <- readings[vari2,1]
-  differ <- num2 - num
-  mydata2 <- data.frame(value=readings[vari2,1],result=differ)
-  mydata <- rbind(mydata,mydata2)
-  vari <- vari+1
-  vari2 <- vari2+1
-  return(mydata)
+for (n in 2:numb) {
+  y <- n-1
+  differ <- readings[n,1] - readings[y,1]
+  tempdata <- data.frame(value=readings[n,1],result=differ)
+  mydata <- rbind(mydata,tempdata)
 }
 
-
-
-for (n in readings) {print(n)}
-
-vari <- 2
-print(readings[vari,])
-print(readings[vari+1,])
-
-vari <- vari+1
-
-num2 <- readings[2,1]+5
-
-loopy
-
+sum(mydata$result > 0)
